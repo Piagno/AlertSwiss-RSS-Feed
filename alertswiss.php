5d032e30c8c3b5391ebb523fe06ea562d5ad607c
Flavio Piagno <20758831+Piagno@users.noreply.github.com>
2020-02-08 22:27:59 +0100

    Add actual functionality

--- alertswiss.php
+++ alertswiss.php
@@ -5,14 +5,9 @@ $url = 'https://alert.swiss/';
 $feed = new feed($url,'ALERTSWISS');
 $feed->author = 'BABS';
 $data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/de/_jcr_content.alertswiss_alerts.json'));
-$alertCount = 0;
-$alertCount = $alertCount + $data->alertCount->severe;
-$alertCount = $alertCount + $data->alertCount->moderate;
-$alertCount = $alertCount + $data->alertCount->minor;
-$alertCount = $alertCount + $data->alertCount->allClear;
-if($alertCount > 0){
-	$item = $feed->newItem('https://alert.swiss','New Alert');
-	$item->content = 'A new Alert appeared please check it out!';
-	$item->link = 'https://alert.swiss';
+foreach($data->alerts as $alert){
+	$item = $feed->newItem($alert->identifier,$alert->title);
+	$item->content = $alert->description;
+	$item->link = $url;
 }
 $feed->printFeed();
