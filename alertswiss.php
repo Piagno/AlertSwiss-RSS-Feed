8d6cc63dffe874414687babe082728efce9580ee
Flavio Piagno <20758831+Piagno@users.noreply.github.com>
2020-02-08 22:37:53 +0100

    Remove technical test alerts from feed

--- alertswiss.php
+++ alertswiss.php
@@ -6,8 +6,10 @@ $feed = new feed($url,'ALERTSWISS');
 $feed->author = 'BABS';
 $data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/de/_jcr_content.alertswiss_alerts.json'));
 foreach($data->alerts as $alert){
-	$item = $feed->newItem($alert->identifier,$alert->title);
-	$item->content = $alert->description;
-	$item->link = $url;
+	if($alert->technicalTestAlert != 'true'){
+		$item = $feed->newItem($alert->identifier,$alert->title);
+		$item->content = $alert->description;
+		$item->link = $url;
+	}
 }
 $feed->printFeed();
