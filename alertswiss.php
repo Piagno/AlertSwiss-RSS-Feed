af20b1e13793c21c34f213a474a7d6a53d79b263
Flavio Piagno <20758831+Piagno@users.noreply.github.com>
2020-02-09 08:56:55 +0100

    ItemID must be an url

--- alertswiss.php
+++ alertswiss.php
@@ -7,7 +7,7 @@ $feed->author = 'BABS';
 $data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/de/_jcr_content.alertswiss_alerts.json'));
 foreach($data->alerts as $alert){
 	if($alert->technicalTestAlert != 'true'){
-		$item = $feed->newItem($alert->identifier,$alert->title);
+		$item = $feed->newItem('https://www.alert.swiss/'.$alert->identifier,$alert->title);
 		$item->content = $alert->description;
 		$item->link = $url;
 	}
