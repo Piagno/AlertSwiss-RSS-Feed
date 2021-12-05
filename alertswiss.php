16264d6ed5f3319255450aab484eacb2acecf955
Flavio Piagno <20758831+Piagno@users.noreply.github.com>
2021-12-05 21:40:47 +0100

    Updated to german feed

--- alertswiss.php
+++ alertswiss.php
@@ -4,7 +4,7 @@ header("Cache-Control: no-cache");
 $url = 'https://alert.swiss/';
 $feed = new feed($url,'ALERTSWISS');
 $feed->author = 'BABS';
-$data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/en/home/_jcr_content/polyalert.alertswiss_alerts.actual.json'));
+$data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/de/home/_jcr_content/polyalert.alertswiss_alerts.actual.json'));
 foreach($data->alerts as $alert){
 	if($alert->technicalTestAlert != 'true'){
 		$item = $feed->newItem('https://www.alert.swiss/'.$alert->identifier,$alert->title);
