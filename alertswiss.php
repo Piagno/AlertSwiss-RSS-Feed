3650e292c60c1a50b8260a66463a678361de08a7
Flavio Piagno <20758831+Piagno@users.noreply.github.com>
2021-12-05 21:38:08 +0100

    Updated link / Escape HTML
    Updated the link to the new feed.
    
    Escaping HTML from content to provide a working feed. (Content won't display properly anymore)
    Reason: Content doesn't include xml decodable html code.

--- alertswiss.php
+++ alertswiss.php
@@ -4,11 +4,11 @@ header("Cache-Control: no-cache");
 $url = 'https://alert.swiss/';
 $feed = new feed($url,'ALERTSWISS');
 $feed->author = 'BABS';
-$data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/de/_jcr_content.alertswiss_alerts.json'));
+$data = json_decode(file_get_contents('https://www.alert.swiss/content/alertswiss-internet/en/home/_jcr_content/polyalert.alertswiss_alerts.actual.json'));
 foreach($data->alerts as $alert){
 	if($alert->technicalTestAlert != 'true'){
 		$item = $feed->newItem('https://www.alert.swiss/'.$alert->identifier,$alert->title);
-		$item->content = $alert->description;
+		$item->content = htmlspecialchars(str_replace('<br>','',$alert->description));
 		$item->link = $url;
 	}
 }
